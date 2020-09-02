4bd50aed65a91730051e90232a0a0db0909996d0
pabloherzberg <pabloherzberg@gmail.com>
2020-09-01 23:50:59 -0300

    piechart

--- src/pages/Geral/index.js
+++ src/pages/Geral/index.js
@@ -7,11 +7,16 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
+  PieChart,
+  Pie,
+  Sector,
+  Cell,
 } from "recharts";
 import { Container, ChartContainer, NavBar } from "./style.js";
 import { TextField, Button } from "@material-ui/core";
 
 function Home() {
+  const [state, setState] = useState({ activeIndex: 0 });
   const [data, setData] = useState();
   const [count, setCount] = useState(0);
   const [totalSetor, setTotalSetor] = useState();
@@ -1298,6 +1303,104 @@ function Home() {
     },
   ];
 
+  const dataPie = [
+    {
+      name: "Atendimentos",
+      value: Number(
+        totalSemana[0] + totalSemana[1] + totalSemana[2] + totalSemana[3]
+      ),
+      fill: "#8884d8",
+    },
+    {
+      name: "Pendências",
+      value: Number(
+        pendenciasSemana[0] +
+          pendenciasSemana[1] +
+          pendenciasSemana[2] +
+          pendenciasSemana[3]
+      ),
+      fill: "#82ca9d",
+    },
+  ];
+
+  const renderActiveShape = (props) => {
+    const RADIAN = Math.PI / 180;
+    const {
+      cx,
+      cy,
+      midAngle,
+      innerRadius,
+      outerRadius,
+      startAngle,
+      endAngle,
+      fill,
+      payload,
+      percent,
+      value,
+    } = props;
+    const sin = Math.sin(-RADIAN * midAngle);
+    const cos = Math.cos(-RADIAN * midAngle);
+    const sx = cx + (outerRadius + 10) * cos;
+    const sy = cy + (outerRadius + 10) * sin;
+    const mx = cx + (outerRadius + 30) * cos;
+    const my = cy + (outerRadius + 30) * sin;
+    const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+    const ey = my;
+    const textAnchor = cos >= 0 ? "start" : "end";
+
+    return (
+      <g>
+        <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>
+          {payload.name}
+        </text>
+        <Sector
+          cx={cx}
+          cy={cy}
+          innerRadius={innerRadius}
+          outerRadius={outerRadius}
+          startAngle={startAngle}
+          endAngle={endAngle}
+          fill={fill}
+        />
+        <Sector
+          cx={cx}
+          cy={cy}
+          startAngle={startAngle}
+          endAngle={endAngle}
+          innerRadius={outerRadius + 6}
+          outerRadius={outerRadius + 10}
+          fill={fill}
+        />
+        <path
+          d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`}
+          stroke={fill}
+          fill="none"
+        />
+        <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
+        <text
+          x={ex + (cos >= 0 ? 1 : -1) * 12}
+          y={ey}
+          textAnchor={textAnchor}
+          fill="#333"
+        >{`Total ${value}`}</text>
+        <text
+          x={ex + (cos >= 0 ? 1 : -1) * 12}
+          y={ey}
+          dy={18}
+          textAnchor={textAnchor}
+          fill="#999"
+        >
+          {`(${(percent * 100).toFixed(2)}%)`}
+        </text>
+      </g>
+    );
+  };
+
+  const onPieEnter = (data, index) => {
+    setState({
+      activeIndex: index,
+    });
+  };
   return (
     <Container>
       <NavBar>
@@ -1320,7 +1423,7 @@ function Home() {
         ></button>
       </NavBar>
       <ChartContainer>
-        <BarChart responsive={true} width={800} height={400} data={chartData}>
+        <BarChart responsive={true} width={600} height={400} data={chartData}>
           <CartesianGrid />
           <XAxis dataKey="name" />
           <YAxis />
@@ -1334,6 +1437,20 @@ function Home() {
             fill="#82ca9d"
           />
         </BarChart>
+        <PieChart width={600} height={400}>
+          <Pie
+            activeIndex={state.activeIndex}
+            activeShape={renderActiveShape}
+            data={dataPie}
+            cx={250}
+            cy={200}
+            innerRadius={60}
+            outerRadius={80}
+            fill="#8884d8"
+            dataKey="value"
+            onMouseEnter={onPieEnter}
+          />
+        </PieChart>
       </ChartContainer>
     </Container>
   );
