eb64c37e2a428d846f5e3380692cd7b89bdb9ea3
pabloherzberg <pabloherzberg@gmail.com>
2021-01-05 19:01:45 -0300

    girar

--- src/pages/Evolucoes/Image/index.js
+++ src/pages/Evolucoes/Image/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useLocation} from 'react-router-dom'
 
 function Image() {
     const location = useLocation()
     const {url} = location.state
-  return <img height="100%" width="100%" src={url} />;
+    const [rotate, setRotate] = useState(0)
+  return (
+      <div>
+          <img style={{transform:`rotate(${rotate}deg)`}} height="100%" width="100%" src={url} />
+          <button onClick={()=>setRotate(rotate - 90)} style={{position:'absolute', bottom:'10%', left:"50%", transform:'translateX(-50%)'}}>Girar</button>
+      </div>
+    );
 }
 
 export default Image;
