5df3bc58ce96e528f8752be488f565de79ec5fc9
pabloherzberg <pabloherzberg@gmail.com>
2021-03-08 10:23:19 -0300

    service worker and update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import {registerServiceWorker} from './serviceWorker'
 import GlobalProvider from "./context/index.js";
 
 ReactDOM.render(
@@ -14,4 +13,3 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-registerServiceWorker()
