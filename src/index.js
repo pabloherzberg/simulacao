12324657667ee6d4f600e9bab170d95b225930b0
pabloherzberg <pabloherzberg@gmail.com>
2021-03-08 11:15:08 -0300

    Revert "service worker and update"
    This reverts commit 5df3bc58ce96e528f8752be488f565de79ec5fc9.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import {registerServiceWorker} from './serviceWorker'
 import GlobalProvider from "./context/index.js";
 
 ReactDOM.render(
@@ -13,3 +14,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
+registerServiceWorker()
