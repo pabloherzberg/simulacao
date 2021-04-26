f14805cdf374f63312391a0640ad8898a58d6f31
Pablo Herzberg <37887593+pabloherzberg@users.noreply.github.com>
2021-04-26 16:18:58 -0300

    Update index.js

--- src/pages/Graficos/index.js
+++ src/pages/Graficos/index.js
@@ -37,13 +37,6 @@ function Home() {
   const [pieChartData2, setPieChartData2] = useState()
 
   useEffect(()=>{
-    const user = JSON.parse(localStorage.getItem("userFono"))||'nada';
-    if(user.email !== ' m.agalhaes@hotmail.com ' || user.email !== ' bobalhao@bobalhao.com '){
-      history.push('/')
-    }
-  },[history])
-
-  useEffect(()=>{
     firebase
       .database()
       .ref(`atendimentos/`)
